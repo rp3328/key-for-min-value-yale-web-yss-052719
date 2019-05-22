f28c1d6e952a1e88ed16936d9152b565b3677b6c
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 02:22:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,5 +8,10 @@ def key_for_min_value(name_hash)
     return nil
   else
     name_hash.each do |key, value|
-      
+      while minv > value
+        mink = key
+      end
+    end
+  end
+  mink
 end
