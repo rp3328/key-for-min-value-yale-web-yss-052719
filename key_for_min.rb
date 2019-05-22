48c83dd0248ca2e3f376022219b43ece3c10efcd
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 02:14:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  mink, minv = name_hash.first
+  
   if name_hash.size == 0
     
 end
